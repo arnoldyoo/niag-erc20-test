3ba95bfec6f34987f36f40339dde6b00ba818c75
arnoldyoo <airnold0986@gmail.com>
2018-10-18 17:11:57 +0900

    complete deploy

--- contracts/NIAG.sol
+++ contracts/NIAG.sol
@@ -17,9 +17,9 @@ contract NIAG is CappedToken(800000000), BurnableToken, NIAGInfo {
 	mapping (address => bool) public frozenAccount;
 
 	event FrozenFunds(address target, bool frozen);
-	event lockUp(address who, uint256 when, uint256 amount);
+	event LockUp(address who, uint256 when, uint256 amount);
 
-  constructor() {
+  constructor() public {
   	totalSupply_ = INITIAL_SUPPLY * 10 ** uint(decimals);
     balances[msg.sender] = INITIAL_SUPPLY;
     owner = msg.sender;
@@ -43,12 +43,12 @@ contract NIAG is CappedToken(800000000), BurnableToken, NIAGInfo {
 	// add lockup
   function lockUp(address _address, uint256 when, uint256 amount) public isOwner returns (bool) {
   	timelockList[_address].push(new NIAGTimelock(this, _address, when, amount));
-		emit lockUp(_address, when, amount);
-		return true
+		emit LockUp(_address, when, amount);
+		return true;
   }
 
 	// release lockup account multi
-	function releaseMulti(address[] memory addresses) {
+	function releaseMulti(address[] memory addresses) public {
   	for(uint i = 0; i < addresses.length; i++) {
     	NIAGTimelock[] memory locks = timelockList[addresses[i]];
       for(uint j = 0; j < locks.length; j++) {
@@ -60,7 +60,7 @@ contract NIAG is CappedToken(800000000), BurnableToken, NIAGInfo {
   }
 
 	// release lockup account single
-  function releaseSingle(address _address) {
+  function releaseSingle(address _address) public {
   	NIAGTimelock[] memory locks = timelockList[_address];
     for(uint j = 0; j < locks.length; j++) {
       if(locks[j].releaseTime() >= block.timestamp) {
@@ -77,25 +77,25 @@ contract NIAG is CappedToken(800000000), BurnableToken, NIAGInfo {
   }
 
 	// single mint
-	function mintSingle(address memory _address, uint256 amount) public biggerThenZero(amount) {
+	function mintSingle(address _address, uint256 amount) public biggerThenZero(amount) {
   	super.mint(_address, amount);
   }
 
 	// burn totalSupply
-  function burn(uint256 _value) isOwner {
+  function burn(uint256 _value) public isOwner {
     require(_value <= totalSupply_);
   	super.burn(_value);
   }
 
 	// @TODO: freeze source
-	function freezeAccount(address memory _address) isOwner {
+	function freezeAccount(address _address) public isOwner {
 			require(frozenAccount[_address] == false);
 			frozenAccount[_address] = true;
 			emit FrozenFunds(_address, true);
 	}
 
 	// unfrozen
-	function unFreezeAccount(address memory _address) isOwner {
+	function unFreezeAccount(address _address) public isOwner {
 		require(frozenAccount[_address] == true);
 		frozenAccount[_address] = false;
 
--- migrations/1539739799_deploy.js
+++ migrations/1539739799_deploy.js
@@ -1,8 +1,5 @@
 const NIAG = artifacts.require("./NIAG");
-const NIAGTimelock = artifacts.require('./NIAGTimelock')
 
 module.exports = function(deployer) {
-  deployer.deploy(NIAG).then(() => {
-      deployer.deploy(NIAGTimelock);
-  })
+  deployer.deploy(NIAG);
 };
