3e22ab9f0e47ff08b81c6f389e63b336b2039585
arnoldyoo <airnold0986@gmail.com>
2018-10-19 15:00:18 +0900

    client test source

--- client/niag-token.js
+++ client/niag-token.js
@@ -5,7 +5,11 @@ const EthereumTx = require('ethereumjs-tx')
 
 const contractAddress = '0x346E87bd11c40393012E488AF296BE47A7a89FBe'
 const abiArray = JSON.parse(fs.readFileSync(path.resolve(__dirname, `./../build/contracts/NIAG.json`), 'utf-8')).abi
-
+const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545'))
+const contract = new web3.eth.Contract(abiArray, contractAddress);
+const privateKey = new Buffer('7482375086b5720a301f49572da24384ce9524cd2bd138b95492a5058d146aef', 'hex');
+const publicKey = '0xe5A019fAb13dBBa56C0C0d695C099bDE1FB76a9B'
+const toAddress = '0x7CA5cb04CFe18B3fcEf5EaCDFf5c3EFE675B1De9'
 
 async function log() {
   const web3 = new Web3(new Web3.providers.WebsocketProvider('http://127.0.0.1:7545'));
@@ -18,127 +22,127 @@ async function log() {
   });
 }
 
+const getBalanceTest = async () => {
+  // owner
+  const balance = await contract.methods.balanceOf(publicKey).call()
+  // account 1
+  // const balance = await contract.methods.balanceOf(toAddress).call()
+
+  console.log('balance : ', balance)
+}
+
+const sendTest = async () => {
+  const sendAmount = 100;
+  const nonceHex = web3.utils.toHex( web3.eth.getTransactionCount(publicKey) );
+  const value = web3.utils.toHex(sendAmount);
+  var count = await web3.eth.getTransactionCount(publicKey);
+  const gasPriceGwei = 2;
+  const gasLimit = 3000000;
+  const chainId = 3;
+  const rawTransaction = {
+      "from": publicKey,
+      "nonce": "0x" + count.toString(16),
+      "gasPrice": web3.utils.toHex(gasPriceGwei * 1e9),
+      "gasLimit": web3.utils.toHex(gasLimit),
+      "to": contractAddress,
+      "value": "0x0",
+      "data": contract.methods.transfer(toAddress, sendAmount).encodeABI(),
+      "chainId": chainId
+  };
+
+  const tx = new EthereumTx(rawTransaction);
+  tx.sign(privateKey);
+  const serializedTx = tx.serialize();
+
+  const result = await web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
+  console.log(result)
+}
+
+const lockTest = async () => {
+  // lock (address, when, amount)
+  const gasPriceGwei = 2;
+  const gasLimit = 6721975;
+  const chainId = 5774;
+  const nonceHex = web3.utils.toHex( web3.eth.getTransactionCount(publicKey) );
+  var count = await web3.eth.getTransactionCount(publicKey);
+  const date = new Date()
+  const rawTransaction = {
+      "from": publicKey,
+      "nonce": "0x" + count.toString(16),
+      "gasPrice": web3.utils.toHex(gasPriceGwei * 1e9),
+      "gasLimit": web3.utils.toHex(gasLimit),
+      "to": contractAddress,
+      "value": "0x0",
+      "data": contract.methods.lockUp(toAddress, new Date().getTime(), 1000).encodeABI(),
+      "chainId": chainId
+  };
+  const tx = new EthereumTx(rawTransaction);
+  tx.sign(privateKey);
+  const serializedTx = tx.serialize();
+  const result = await web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
+  console.log(result)
+}
+
+const releaseTest = async () => {
+  const gasPriceGwei = 3;
+  const gasLimit = 6721975;
+  const chainId = 5774;
+  const nonceHex = web3.utils.toHex( web3.eth.getTransactionCount(publicKey) );
+  const count = await web3.eth.getTransactionCount(publicKey);
+  const rawTransaction = {
+      "from": publicKey,
+      "nonce": "0x" + count.toString(16),
+      "gasPrice": web3.utils.toHex(gasPriceGwei * 1e9),
+      "gasLimit": web3.utils.toHex(gasLimit),
+      "to": contractAddress,
+      "value": "0x0",
+      "data": contract.methods.release(toAddress).encodeABI(),
+      "chainId": chainId
+  };
+  const tx = new EthereumTx(rawTransaction);
+  tx.sign(privateKey);
+  const serializedTx = tx.serialize();
+
+  const result = await web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
+  console.log(result)
+}
+
+const freezeTest = async () => {
+  const gasPriceGwei = 3;
+  const gasLimit = 6721975;
+  const chainId = 5774;
+  const nonceHex = web3.utils.toHex( web3.eth.getTransactionCount(publicKey) );
+  const count = await web3.eth.getTransactionCount(publicKey);
+  const rawTransaction = {
+      "from": publicKey,
+      "nonce": "0x" + count.toString(16),
+      "gasPrice": web3.utils.toHex(gasPriceGwei * 1e9),
+      "gasLimit": web3.utils.toHex(gasLimit),
+      "to": contractAddress,
+      "value": "0x0",
+      "data": contract.methods.freezeAccount(toAddress).encodeABI(),
+      "chainId": chainId
+  };
+  const tx = new EthereumTx(rawTransaction);
+  tx.sign(privateKey);
+  const serializedTx = tx.serialize();
+
+  const result = await web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
+  console.log(result)
+}
+
 
 async function main() {
-  const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545'))
-  const contract = new web3.eth.Contract(abiArray, contractAddress);
+  getBalanceTest()
 
-
-  // transfer
-  // const sendAmount = 100;
-  // const privateKey = new Buffer('4499ef873d42c5e6c631abc5e6caac93527e9aa4ddcd048b95d5a1b517e7b0cf', 'hex');
-  // const publicKey = '0x331833dc72688Ff77C2cc761C18F105EdA24084d'
-  // const toAddress = '0x01a2FA8d96436d8f962DBc2AE6c5D5316D51ED58'
-  // const nonceHex = web3.utils.toHex( web3.eth.getTransactionCount(publicKey) );
-  // const value = web3.utils.toHex(sendAmount);
-  // var count = await web3.eth.getTransactionCount(publicKey);
-  // const gasPriceGwei = 2;
-  // const gasLimit = 3000000;
-  // const chainId = 3;
-  // const rawTransaction = {
-  //     "from": publicKey,
-  //     "nonce": "0x" + count.toString(16),
-  //     "gasPrice": web3.utils.toHex(gasPriceGwei * 1e9),
-  //     "gasLimit": web3.utils.toHex(gasLimit),
-  //     "to": contractAddress,
-  //     "value": "0x0",
-  //     "data": contract.methods.transfer(toAddress, sendAmount).encodeABI(),
-  //     "chainId": chainId
-  // };
+  // sendTest()
   //
-  // const tx = new EthereumTx(rawTransaction);
-  // tx.sign(privateKey);
-  // const serializedTx = tx.serialize();
+  // lockTest()
   //
-  // const result = await web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
-  // console.log(result)
-
-
-  // get balance
-  // owner 0x331833dc72688Ff77C2cc761C18F105EdA24084d
-  // const balance = await contract.methods.balanceOf('0xe5A019fAb13dBBa56C0C0d695C099bDE1FB76a9B').call()
-  // account 1
-  const balance = await contract.methods.balanceOf('0x7CA5cb04CFe18B3fcEf5EaCDFf5c3EFE675B1De9').call()
-  console.log('balance : ', balance)
-
-  // lock (address, when, amount)
-  // const gasPriceGwei = 2;
-  // const gasLimit = 6721975;
-  // const chainId = 5774;
-  // const privateKey = new Buffer('7482375086b5720a301f49572da24384ce9524cd2bd138b95492a5058d146aef', 'hex');
-  // const publicKey = '0xe5A019fAb13dBBa56C0C0d695C099bDE1FB76a9B'
-  // const toAddress = '0x7CA5cb04CFe18B3fcEf5EaCDFf5c3EFE675B1De9'
-  // const nonceHex = web3.utils.toHex( web3.eth.getTransactionCount(publicKey) );
-  // var count = await web3.eth.getTransactionCount(publicKey);
-  // const date = new Date()
-  // const rawTransaction = {
-  //     "from": publicKey,
-  //     "nonce": "0x" + count.toString(16),
-  //     "gasPrice": web3.utils.toHex(gasPriceGwei * 1e9),
-  //     "gasLimit": web3.utils.toHex(gasLimit),
-  //     "to": contractAddress,
-  //     "value": "0x0",
-  //     "data": contract.methods.lockUp(toAddress, new Date().getTime(), 1000).encodeABI(),
-  //     "chainId": chainId
-  // };
-  // const tx = new EthereumTx(rawTransaction);
-  // tx.sign(privateKey);
-  // const serializedTx = tx.serialize();
-  // const result = await web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
-  // console.log(result)
-
-  // release(address)
-  // const privateKey = new Buffer('7482375086b5720a301f49572da24384ce9524cd2bd138b95492a5058d146aef', 'hex');
-  // const publicKey = '0xe5A019fAb13dBBa56C0C0d695C099bDE1FB76a9B'
-  // const toAddress = '0x7CA5cb04CFe18B3fcEf5EaCDFf5c3EFE675B1De9'
-  // const gasPriceGwei = 3;
-  // const gasLimit = 6721975;
-  // const chainId = 5774;
-  // const nonceHex = web3.utils.toHex( web3.eth.getTransactionCount(publicKey) );
-  // const count = await web3.eth.getTransactionCount(publicKey);
-  // const rawTransaction = {
-  //     "from": publicKey,
-  //     "nonce": "0x" + count.toString(16),
-  //     "gasPrice": web3.utils.toHex(gasPriceGwei * 1e9),
-  //     "gasLimit": web3.utils.toHex(gasLimit),
-  //     "to": contractAddress,
-  //     "value": "0x0",
-  //     "data": contract.methods.release(toAddress).encodeABI(),
-  //     "chainId": chainId
-  // };
-  // const tx = new EthereumTx(rawTransaction);
-  // tx.sign(privateKey);
-  // const serializedTx = tx.serialize();
+  // releaseTest()
   //
-  // const result = await web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
-  // console.log(result)
-
-  // freeze
-  // const privateKey = new Buffer('7482375086b5720a301f49572da24384ce9524cd2bd138b95492a5058d146aef', 'hex');
-  // const publicKey = '0xe5A019fAb13dBBa56C0C0d695C099bDE1FB76a9B'
-  // const toAddress = '0x7CA5cb04CFe18B3fcEf5EaCDFf5c3EFE675B1De9'
-  // const gasPriceGwei = 3;
-  // const gasLimit = 6721975;
-  // const chainId = 5774;
-  // const nonceHex = web3.utils.toHex( web3.eth.getTransactionCount(publicKey) );
-  // const count = await web3.eth.getTransactionCount(publicKey);
-  // const rawTransaction = {
-  //     "from": publicKey,
-  //     "nonce": "0x" + count.toString(16),
-  //     "gasPrice": web3.utils.toHex(gasPriceGwei * 1e9),
-  //     "gasLimit": web3.utils.toHex(gasLimit),
-  //     "to": contractAddress,
-  //     "value": "0x0",
-  //     "data": contract.methods.freezeAccount(toAddress).encodeABI(),
-  //     "chainId": chainId
-  // };
-  // const tx = new EthereumTx(rawTransaction);
-  // tx.sign(privateKey);
-  // const serializedTx = tx.serialize();
-  //
-  // const result = await web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
-  // console.log(result)
-
+  // freezeTest()
 }
+
 log()
 main()
